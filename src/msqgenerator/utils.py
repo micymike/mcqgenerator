09281834bc4c834a33c8d6b037ad2351ab568154
micymike <mosesmichael878@gmail.com>
2024-06-09 11:16:59 +0300

    final commit

---
+++ src/msqgenerator/utils.py
@@ -0,0 +1,39 @@
+import os
+import json
+import traceback
+import PyPDF2
+
+def read_file(file):
+    if file.name.endswith(".pdf"):
+        try:
+            pdf_reader = PyPDF2.PdfFileReader(file)
+            text = ""
+            for page_num in range(pdf_reader.getNumPages()):
+                page = pdf_reader.getPage(page_num)
+                text += page.extractText()
+            return text
+        except Exception as e:
+            raise Exception("Error reading the PDF file: " + str(e))
+    elif file.name.endswith(".txt"):
+        return file.read().decode("utf-8")
+    else:
+        raise Exception("Unsupported file format. Only PDF and text files are supported.")
+
+def get_table_data(quiz_str):
+    try:
+        quiz_dict = json.loads(quiz_str)
+        quiz_table_data = []
+
+        for key, value in quiz_dict.items():
+            mcq = value["mcq"]
+            options = " || ".join(
+                [f"{option} -> {option_value}" for option, option_value in value["options"].items()]
+            )
+            correct = value["correct"]
+            quiz_table_data.append({"MCQ": mcq, "Choices": options, "Correct": correct})
+
+        return quiz_table_data
+
+    except Exception as e:
+        traceback.print_exception(type(e), e, e.__traceback__)
+        return False
